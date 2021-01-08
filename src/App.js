8d9454d1aec19971ba8456f0f047551332bc017e
YJH327 <didwnsgh327@gmail.com>
2021-01-08 21:11:04 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import './App.css';
+import React from 'react';
 import Customer from './components/Customer'
+import Paper from "@material-ui/core/Paper"
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
 
 const customers = [
   {
@@ -16,7 +49,7 @@ const customers = [
     'name' : '양준호',
     'birthday' : '980327',
     'gender' : '남자',
-    'job' : '대학생'
+    'job' : '디자이너'
   },
   {
     'id' : 3,
@@ -24,21 +57,31 @@ const customers = [
     'name' : '동빈나',
     'birthday' : '980327',
     'gender' : '남자',
-    'job' : '대학생'
+    'job' : '프로그래머'
   }
 ]
 
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      { customers.map(c => {
-          return (
-            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-          )
-        })
-      }
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>번호</StyledTableCell>
+            <StyledTableCell>이미지</StyledTableCell>
+            <StyledTableCell>이름</StyledTableCell>
+            <StyledTableCell>생년월일</StyledTableCell>
+            <StyledTableCell>성별</StyledTableCell>
+            <StyledTableCell>직업</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { customers.map(c => {return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
